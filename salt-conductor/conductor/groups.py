2011b435ae1fc1cdb4a8a7991de93fe5c09396e1
Paul Bruno <15909838+thetoolsmith@users.noreply.github.com>
2020-01-23 21:51:12 -0500

    clean out specific data

--- salt-conductor/conductor/groups.py
+++ salt-conductor/conductor/groups.py
@@ -14,11 +14,8 @@ class Products(object):
   CLOUD = 'CLOUD'
   DEVOPS = 'DEVOPS'
   SALTY = 'SALTY' #salt dev testing 
-  POLARIS = 'POLARIS'
-  DATAMART = 'DATAMART'
-  METRAS = 'METRAS'
-  NEXUS = 'NEXUS'
-  supported = [CLOUD, DEVOPS, METRAS, SALTY]
+  
+  supported = [CLOUD, DEVOPS, SALTY]
   cluster_internal_roles = ['primary', 'secondary', 'seed', 'arbiter', 'coordinator']
 
 class Providers(object):
@@ -151,95 +148,5 @@ class DEVOPS(object):
                     "volume_info": []}
 
 
-class METRAS(object):
 
-  def __init__(self):
-    '''
-    '''
-    self.nodes = []
-    self.instances = []
-    self.new_cloud_build = {}
-    self.new_cloud_build_xtra = []
-
-  class Conf(object):
-    delaystates = [] 
-    node_meta = [] #list of dict objects where key=nodename value=metadata
-    all_nodes = [] #collection of all nodes of each type being created in this instance 
-    default_startup = None
-    root_volume_tags = {}
-
-  class PreConfig(object):
-    cluster_member_count = 0
-    cluster_members = []
-
-  class Prebuild(object):
-    '''
-    Component data strutcure used by submodules to create cloud conf files 
-    needs a constructor because we create many of these in one instance of Conductor.METRAS
-    '''
-    def __init__(self):
-      self.component = { "iscluster": False,
-                    "cluster-config": {},
-                    "nodes": 0,
-                    "names": [],
-                    "pattern": None,
-                    "size": None,
-                    "role": None,
-                    "roleid": None,
-                    "baserole": None,
-                    "compositerole": [],
-                    "startupoverride": [],
-                    "ami_override": None,
-                    "internalrole": None,
-                    "tags": [],
-                    "force_delay_state": False,
-                    "ebs_optimize": False,
-                    "root_volume_info": {},
-                    "volume_info": []}
-
-class POLARIS(object):
-
-  def __init__(self):
-    '''
-    '''
-    self.nodes = []
-    self.instances = []
-    self.new_cloud_build = {}
-    self.new_cloud_build_xtra = []
-
-  class Conf(object):
-    delaystates = [] 
-    node_meta = [] #list of dict objects where key=nodename value=metadata
-    all_nodes = [] #collection of all nodes of each type being created in this instance 
-    default_startup = None
-    root_volume_tags = {}
-
-  class PreConfig(object):
-    #for each cluster being created, create a class of this and set cluster member count for use in the cloud map
-    cluster_member_count = 0
-    cluster_members = []
-
-  class Prebuild(object):
-    '''
-    Component data strutcure used by submodules to create cloud conf files 
-    needs a constructor because we create many of these in one instance of Conductor.polaris
-    '''
-    def __init__(self):
-      self.component = { "iscluster": False,
-                    "cluster-config": {},
-                    "nodes": 0,
-                    "names": [],
-                    "pattern": None,
-                    "size": None,
-                    "role": None,
-                    "roleid": None,
-                    "baserole": None,
-                    "compositerole": [],
-                    "startupoverride": [],
-                    "ami_override": None,
-                    "internalrole": None,
-                    "force_delay_state": False,
-                    "ebs_optimize": False,
-                    "root_volume_info": {},
-                    "volume_info": []}
 
